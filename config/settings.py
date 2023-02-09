2575c1658dea005bf0bb58772a23096c9aa6af00
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-02-09 19:16:53 +0200

    edited: added settings for media

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+# STATIC config
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# STATIC config end
+
 
 # Url config
 LOGIN_REDIRECT_URL = 'dashboard'

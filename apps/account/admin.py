52485e65ef4c2ef210946ed060e15a399e913cce
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-02-09 19:24:46 +0200

    edited: added model Profile with fields to admin
     - user
     - date_of_birth
     - photo

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# Local
+from .models import Profile
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']

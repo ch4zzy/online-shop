c555e2ef1f67ca6228980eff7b2e821c0be48dde
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-02-10 16:59:38 +0200

    edited: created edit func

--- apps/account/views.py
+++ apps/account/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 # Local
-from .forms import LoginForm, UserRegistration
+from .forms import LoginForm, UserRegistration, UserEditForm, ProfileEditForm
 from .models import Profile
 # Create your views here.
 
@@ -56,4 +56,26 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
+    )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES,
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {'user_form': user_form, 'profile_form': profile_form}
     )

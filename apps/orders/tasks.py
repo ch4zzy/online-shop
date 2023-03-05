1e5135e29c724bfcc6bb5aa894e1b03b4d4f9261
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-05 19:37:19 +0200

    edited: added email sent

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -4,3 +4,15 @@ from django.core.mail import send_mail
 from .models import Order
 
 
+@task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Order nr. {order.id}'
+    message = f'Dear {order.first_name},\n\n You have successfully placed an order.\
+                Your order id is {order.id}.'
+    mail_sent = send_mail(
+        subject, 
+        message,
+        'admin@marketplace.com',
+        [order.email])
+    return mail_sent

4c805f1ba3281c36c95ec926ddb57cc189c0a833
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-05 20:04:08 +0200

    edited: changed app name

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('marketplace')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()

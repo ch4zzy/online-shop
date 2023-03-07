60d02138738e15518594ebd0ac252b5d1e765799
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-07 19:02:04 +0200

    edited: added payment function

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+import braintree
 
+# Local
+from apps.orders.models import Order
 # Create your views here.
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == 'POST':
+        nonce = braintree.Transaction.sale({
+            'amount': '{:.2f}'.format(order.get_total_cost()),
+            'payment_method_nonce': nonce,
+            'options': {
+                'submit_for_settlement': True
+            }
+        })
+
+        if result.is_success:
+            order.paid = True
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        client_token = braintree.ClientToken.generate()
+        return render(request,
+                      'payment/process.html',
+                      {'order': order,
+                       'client_token': client_token})

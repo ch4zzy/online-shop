617a486e7949b4e18f5588af33aa06c619b6efb2
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-10 16:56:14 +0200

    edited: added path for list_display to order_detail

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -44,7 +44,7 @@ export_to_csv.short_description = 'Export to CSV'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]

c1423d5c702e342643ca37b5ab21fbf867cdcf7c
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-10 16:07:28 +0200

    edited: added func order_detail_view

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 # Local
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 from apps.cart.cart import Cart
@@ -29,3 +31,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 
+                  'admin/orders/order/detail.html',
+                  {'order': order})

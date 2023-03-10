97d432753e01ee9b91a5b3133409f695058667be
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-10 17:09:30 +0200

    edited: fix an error

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('orders/', views.order_create, name='order_create'),
-    path('admin/order/', views.admin_order_detail, name='admin_order_detail'),
+    path('create/', views.order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
 ]

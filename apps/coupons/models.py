2235bea5c4cffdeb859690c160c8d6a732e9ca8e
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-12 14:44:03 +0200

    edited: added coupon model

--- apps/coupons/models.py
+++ apps/coupons/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    active = models.BooleanField()
+
+    class Meta:
+        ordering = ('-valid_from', )
+
+    def __str__(self):
+        return self.code

147482a8748fbe8e653a2b8d243d548cd10f29c7
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-15 14:35:42 +0200

    edited: changed method, added discount

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -33,8 +33,9 @@ class Order(models.Model):
         return f'Order {self.id}'
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
-
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
+    
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,

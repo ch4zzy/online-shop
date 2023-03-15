6880609b63090a60d6d060bc422a9df8e272de97
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-15 20:48:41 +0200

    edited: added form for comments

---
+++ apps/shop/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+# Local
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')

ba3f5c6fbe6b0a637d650d728ad93cd932672aa3
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-16 19:26:36 +0200

    edited: fix error with rabbitmq and celery
     - Celery - errno 111 connection refused

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('config')
+app = Celery('config', broker='amqp://rabbitmq')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()

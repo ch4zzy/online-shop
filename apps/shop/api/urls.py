13753a1742a885bd27891165120cc1b910863703
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-23 15:58:16 +0200

    edited: added path for category delete

--- apps/shop/api/urls.py
+++ apps/shop/api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('category/', views.CategoryListView.as_view(), name='category_list'),
     path('category/', views.CategoryDetailView.as_view(), name='category_detail'),
     path('category/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('category/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
     path('product/', views.ProductListView.as_view(), name='product_list'),
     path('product/', views.ProductDetailView.as_view(), name='product_detail'),
 ]

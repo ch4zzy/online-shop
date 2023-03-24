79b08c09c7ae91282f1e2332fe85e5c8985e2975
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-24 20:16:45 +0200

    edited: added serializer for product

--- apps/shop/api/serializers.py
+++ apps/shop/api/serializers.py
@@ -28,3 +28,22 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'category', 'name', 'slug', 'description', 'price', 'available', )
+
+class ProductSerializer2(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField(max_length=255)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+    category_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.slug = validated_data.get("slug", instance.slug)
+        instance.created = validated_data.get("created", instance.created)
+        instance.updated = validated_data.get("updated", instance.updated)
+        instance.save()
+        return instance

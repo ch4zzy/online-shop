9da915b0e985cbf8c281fe2e608c4ca763b3153d
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-24 16:39:19 +0200

    edited: added update function

--- apps/shop/api/serializers.py
+++ apps/shop/api/serializers.py
@@ -12,10 +12,16 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class CategorySerializer2(serializers.Serializer):
     name = serializers.CharField(max_length=255)
-    slug = slugify(name).lower()
+    slug = serializers.SlugField(max_length=255)
 
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.slug = validated_data.get("slug", instance.slug)
+        instance.save()
+        return instance
 
 
 class ProductSerializer(serializers.ModelSerializer):

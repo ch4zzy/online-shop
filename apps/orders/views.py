e04415b7433dba646336c287405a2a958f23d028
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-03-28 16:24:53 +0300

    edited: added only login order create

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -16,25 +16,29 @@ from apps.cart.cart import Cart
 
 def order_create(request):
     cart = Cart(request)
-    if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            if cart.coupon:
-                order.coupon = cart.coupon
-                order.discount = cart.coupon.discount
-            order.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
-            cart.clear()
-            order_created.delay(order.id)
-            request.session['order_id'] = order.id
-            return redirect(reverse('payment:process'))
-    else:
-        form = OrderCreateForm()
+    current_user = request.user
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            form = OrderCreateForm(request.POST)
+            if form.is_valid():
+                order = form.save(commit=False)
+                if cart.coupon:
+                    order.coupon = cart.coupon
+                    order.discount = cart.coupon.discount
+                order.save()
+                for item in cart:
+                    OrderItem.objects.create(order=order,
+                                            product=item['product'],
+                                            price=item['price'],
+                                            quantity=item['quantity'])
+                cart.clear()
+                order_created.delay(order.id)
+                request.session['order_id'] = order.id
+                return redirect(reverse('payment:process'))
+        else:
+            form = OrderCreateForm()
+    else: 
+        return redirect('login')
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
@@ -57,5 +61,5 @@ def admin_order_pdf(request, order_id):
     response['Content-Disposition'] = f'filename="order_{order.id}.pdf"'
     weasyprint.HTML(string=html).write_pdf(response, 
                                            stylesheets=[weasyprint.CSS(
-                                                settings.STATIC_ROOT + 'css/pdf.css')])
+                                                settings.STATIC_PDF + '/css/pdf.css')])
     return response

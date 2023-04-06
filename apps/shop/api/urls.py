793426cf779134695fdc5bf72fa7ef43d5c5c1a7
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-06 14:57:47 +0300

    edited: deleted unnecessary

--- apps/shop/api/urls.py
+++ apps/shop/api/urls.py
@@ -9,16 +9,6 @@ app_name = 'shop'
 urlpatterns = [
     path('category/', views.CategoryAPIView.as_view()),
     path('category/<int:pk>/', views.CategoryAPIView.as_view()),
-
     path('product/', views.ProductAPIView.as_view()),
     path('product/<int:pk>', views.ProductAPIView.as_view()),
-
-    
-    #path('category/', views.CategoryListView.as_view(), name='category_list'),
-    #path('category/', views.CategoryDetailView.as_view(), name='category_detail'),
-    #path('category/create/', views.CategoryCreateView.as_view(), name='category_create'),
-    #path('category/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
-    #path('product/', views.ProductListView.as_view(), name='product_list'),
-    #path('product/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
-

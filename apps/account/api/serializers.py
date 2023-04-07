a080bbf0787fe2a8b33ab8a035501b077820353e
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-07 17:23:41 +0300

    edited: optimize api for account

--- apps/account/api/serializers.py
+++ apps/account/api/serializers.py
@@ -1,22 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-# Local
-from ..models import Profile
 
-class ProfileSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=255)
-    first_name = serializers.CharField(max_length=255)
-    last_name = serializers.CharField(max_length=255)
-    email = serializers.EmailField(max_length=255)
-    is_staff = serializers.BooleanField(default=False)
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
-        
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get("username", instance.username)
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.email = validated_data.get("email", instance.email)
-        instance.is_staff = validated_data.get("is_staff", instance.is_staff)
-        instance.save()
-        return instance
+
+class AccountSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = "__all__"
--- apps/account/api/views.py
+++ apps/account/api/views.py
@@ -1,45 +1,12 @@
-from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, viewsets
 from django.contrib.auth.models import User
 
 # Local
-from .serializers import ProfileSerializer
+from .serializers import AccountSerializer
+from .permissions import IsAdminOrReadOnly
 
 
-class ProfileAPIView(APIView):
-    def get(self, request):
-        profile = User.objects.all()
-        return Response({"profile": ProfileSerializer(profile, many=True).data,})
-
-    def post(self, request):
-        serializer = ProfileSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"profile": serializer.data})
-    
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.pk("pk", None)
-        if not pk:
-            return Response({"error": "Method delete not allowed"})
-        try:
-            instance = User.objects.get(pk=pk)
-            instance.delete()
-        except:
-            return Response({"error": "Object does not exist"})
-        return Response({"profile": "Successfulpy delete" + str(pk)})
-
-    def put(self, request, *args, **kwargs):
-        pk =kwargs.pk("pk", None)
-        if not pk:
-            return Response({"error": "Method put not allowed"})
-        try:
-            instance = User.objects.get(pk=pk)
-        except:
-            return Response({"error": "Method put not allowed"})
-        
-        serializer = ProfileSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"profile": serializer.data})
-    
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = (IsAdminOrReadOnly, )

640f617307288688d127e1dfe1dcec829b10ccf8
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-07 16:24:36 +0300

    edited: added permissions for views

--- apps/shop/api/views.py
+++ apps/shop/api/views.py
@@ -9,13 +9,16 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly, )

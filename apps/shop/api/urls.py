cc81093b02bd5795071795ad24db6aeb6a9d3203
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-10 15:20:58 +0300

    edited: added path for swagger docs

--- apps/shop/api/urls.py
+++ apps/shop/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 # Local
 from . import views
@@ -10,7 +11,10 @@ router.register(r'category', views.CategoryViewSet)
 router.register(r'product', views.ProductViewSet)
 router.register(r'comment', views.CommentViewSet)
 
+api_view = get_swagger_view(title="Api")
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path(r'api-docs/', api_view),
 ]

e4fd2736854227cbb5a782b08d513d03f5edcbe5
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-10 15:29:26 +0300

    edited: added swagger docs

--- apps/account/api/urls.py
+++ apps/account/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers, urls
+from rest_framework_swagger.views import get_swagger_view
 
 # Local
 from . import views
@@ -7,6 +8,9 @@ app_name = 'account'
 router = routers.SimpleRouter()
 router.register(r'account', views.AccountViewSet)
 
+api_view = get_swagger_view(title="Api")
+
 urlpatterns = [
     path('', include(router.urls)),
+    path(r'api-docs/', api_view),
 ]
--- apps/orders/api/urls.py
+++ apps/orders/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 # Local
 from . import views
@@ -8,7 +9,10 @@ app_name = 'orders'
 router = routers.SimpleRouter()
 router.register(r'order', views.OrderViewSet)
 
+api_view = get_swagger_view(title="Api")
+
 urlpatterns = [
     path('', include(router.urls)),
+    path(r'api-docs/', api_view),
 ]
 

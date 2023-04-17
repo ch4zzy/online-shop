ba3b61c75574175037648c9a00dfc49ce4a9a78f
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-04-17 15:26:07 +0300

    edited: created docstrings for api orders app

--- apps/orders/api/serializers.py
+++ apps/orders/api/serializers.py
@@ -5,7 +5,9 @@ from apps.orders.models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
-
+    """
+    A serializer for the Order model.
+    """
     class Meta:
         model = Order
         fields = "__all__"
--- apps/orders/api/views.py
+++ apps/orders/api/views.py
@@ -7,6 +7,24 @@ from apps.orders.api.permissions import IsAdminOrReadOnly
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides CRUD operations for orders.
+
+    Methods:
+    - GET: Retrieve one or many orders.
+    - POST: Create a new order.
+    - PUT: Update an existing order.
+    - DELETE: Delete an order.
+
+    Attributes:
+    - queryset: The queryset of orders to operate on.
+    - serializer_class: The serializer to use for (de)serializing orders.
+    - permission_classes: The permission classes to use for accessing the viewset.
+
+    Permissions:
+    - Users with admin privileges can perform any operation.
+    - Other users can only retrieve orders (read-only).
+    """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAdminOrReadOnly, )

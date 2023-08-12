ba4296538d06dc3c7051bab176acf91c31f8e3ed
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-08-12 17:44:10 +0300

    formatted: pre-commit

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-# Local
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("orders/", include("apps.orders.urls", namespace="orders")),
